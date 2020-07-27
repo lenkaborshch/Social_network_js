2baa9394f493d5a970c4e39b9ece2a7c88bc7bfd
Helen <melnikelena6969@gmail.com>
2020-07-27 22:43:00 +0300

    55. Pagination (users)

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,23 +1,32 @@
 import React from "react"
 import {connect} from "react-redux";
-import {followAC, setUsersAC, unfollowAC} from "../../redux/usersReducer";
+import {followAC, setCurrentPageAC, setTotalUsersCountAC, setUsersAC, unfollowAC} from "../../redux/usersReducer";
 import Users from "./Users";
 
 let mapStateToProps = (state) => ({
-    users: state.usersPage.users
+    users: state.usersPage.users,
+    pageSize: state.usersPage.pageSize,
+    totalUsersCount: state.usersPage.totalUsersCount,
+    currentPage: state.usersPage.currentPage
 })
 
 let mapDispatchToProps = (dispatch) => ({
-        follow: (userId) => {
-            dispatch(followAC(userId))
-        },
-        unfollow: (userId) => {
-            dispatch(unfollowAC(userId))
-        },
-        setUsers: (users) => {
-            dispatch(setUsersAC(users))
-        }
-    })
+    follow: (userId) => {
+        dispatch(followAC(userId))
+    },
+    unfollow: (userId) => {
+        dispatch(unfollowAC(userId))
+    },
+    setUsers: (users) => {
+        dispatch(setUsersAC(users))
+    },
+    setCurrentPage: (pageNumber) => {
+        dispatch(setCurrentPageAC(pageNumber))
+    },
+    setTotalUsersCount: (totalCount) => {
+        dispatch(setTotalUsersCountAC(totalCount))
+    }
+})
 
 
 let UsersContainer = connect(mapStateToProps, mapDispatchToProps)(Users)
